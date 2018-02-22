ae1eef25b468ac340d1bcc259f19811fa2910c8e
KwonHyunwoo <khw6428@gmail.com>
2018-02-23 02:35:18 +0900

    Amend Explanation

--- src/com/company/kwonhyunwoo/Intro/SortByHeight.java
+++ src/com/company/kwonhyunwoo/Intro/SortByHeight.java
@@ -3,28 +3,28 @@ package com.company.kwonhyunwoo.Intro;
 import java.util.Arrays;
 
 /**
- * Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
- * <p>
- * Example
- * <p>
- * For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
- * sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
- * <p>
- * Input/Output
- * <p>
- * [execution time limit] 3 seconds (java)
- * <p>
- * [input] array.integer a
- * <p>
- * If a[i] = -1, then the ith position is occupied by a tree. Otherwise a[i] is the height of a person standing in the ith position.
- * <p>
- * Guaranteed constraints:
- * 5 ≤ a.length ≤ 15,
- * -1 ≤ a[i] ≤ 200.
- * <p>
- * [output] array.integer
- * <p>
- * Sorted array a with all the trees untouched.
+ Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
+
+ Example
+
+ For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
+ sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
+
+ Input/Output
+
+ [execution time limit] 3 seconds (java)
+
+ [input] array.integer a
+
+ If a[i] = -1, then the ith position is occupied by a tree. Otherwise a[i] is the height of a person standing in the ith position.
+
+ Guaranteed constraints:
+ 5 ≤ a.length ≤ 15,
+ -1 ≤ a[i] ≤ 200.
+
+ [output] array.integer
+
+ Sorted array a with all the trees untouched.
  */
 public class SortByHeight {
     public static void main(String[] args) {
