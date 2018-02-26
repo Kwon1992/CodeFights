70baafc80bc86792f4d7f3c6aae663781f1d9e17
KwonHyunwoo <khw6428@gmail.com>
2018-02-27 01:17:45 +0900

    Add explanation, question, and condition

--- src/com/company/kwonhyunwoo/Intro/AllLongestStrings.java
+++ src/com/company/kwonhyunwoo/Intro/AllLongestStrings.java
@@ -6,27 +6,27 @@ import java.util.List;
 
 /**
  * Given an array of strings, return another array containing all of its longest strings.
- * <p>
- * Example
- * <p>
- * For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
- * allLongestStrings(inputArray) = ["aba", "vcd", "aba"].
- * <p>
- * Input/Output
- * <p>
- * [execution time limit] 3 seconds (java)
- * <p>
- * [input] array.string inputArray
- * <p>
- * A non-empty array.
- * <p>
- * Guaranteed constraints:
- * 1 ≤ inputArray.length ≤ 10,
- * 1 ≤ inputArray[i].length ≤ 10.
- * <p>
- * [output] array.string
- * <p>
- * Array of the longest strings, stored in the same order as in the inputArray.
+
+ Example
+
+ For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
+ allLongestStrings(inputArray) = ["aba", "vcd", "aba"].
+
+ Input/Output
+
+ [execution time limit] 3 seconds (java)
+
+ [input] array.string inputArray
+
+ A non-empty array.
+
+ Guaranteed constraints:
+ 1 ≤ inputArray.length ≤ 10,
+ 1 ≤ inputArray[i].length ≤ 10.
+
+ [output] array.string
+
+ Array of the longest strings, stored in the same order as in the inputArray.
  */
 public class AllLongestStrings {
 
--- src/com/company/kwonhyunwoo/Intro/ArrayChange.java
+++ src/com/company/kwonhyunwoo/Intro/ArrayChange.java
@@ -1,5 +1,32 @@
 package com.company.kwonhyunwoo.Intro;
 
+/**
+ * You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+
+ Example
+
+ For inputArray = [1, 1, 1], the output should be
+ arrayChange(inputArray) = 3.
+
+ Input/Output
+
+ [execution time limit] 3 seconds (java)
+
+ [input] array.integer inputArray
+
+ Guaranteed constraints:
+ 3 ≤ inputArray.length ≤ 105,
+ -105 ≤ inputArray[i] ≤ 105.
+
+ [output] integer
+
+ The minimal number of moves needed to obtain a strictly increasing sequence from inputArray.
+ It's guaranteed that for the given test cases the answer always fits signed 32-bit integer type.
+
+ Here is an explanation for test [-1000, 0, -2, 0]: it can be easily transformed using 5 operations as follows:
+ [-1000, 0, -2, 0] -> [-1000, 0, -1, 0] -> [-1000, 0, 0, 0] -> [-1000, 0, 1, 0] -> [-1000, 0, 1, 1] -> [-1000, 0, 1, 2]
+ Hope it'll help"
+ */
 public class ArrayChange {
     public static void main(String[] args) {
         System.out.println(arrayChange(new int[]{1, 1, 1}));
